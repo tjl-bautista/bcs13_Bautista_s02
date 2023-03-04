f79861c4dbee14a0941575c19015a2819aed8163
tjl-bautista <btl1223@dlsud.edu.ph>
2023-03-04 13:05:44 +0800

    Add Data Types and Variables to Python

---
+++ discussion.py
@@ -0,0 +1,58 @@
+#Comments in python are done using '#' symbol
+#reminder that comments are not read by the program and only by the user
+#comments can be done in line as well
+
+#python syntax
+#Hello World in Python
+print("Hello World")
+# print() is a function that prints the contents of the function to the terminal
+
+# Indentation is very important, it is used to indicate a block code. Similar to JS, there is no need to end the statement with semicolon
+
+#Mini-Activity
+#Display your answer using the print() 
+#Name
+#Course
+#At least 5 hobbies 
+#perform git operations in every changes/updates in file
+#screenshot the block code together with the result and paste it in our SB later
+
+
+
+user_name = "Name: Timothy James L. Bautista"
+user_course = "Course: BS Computer Science"
+user_hobbies = "Hobbies: My hobbies include playing video games, watching anime, listening to music and playing guitar, experimenting with coding, and learning more about technology"
+
+print(user_name + "\n" + user_course + "\n" + user_hobbies)
+
+num1 = 3                    #integer
+grade = 98.5                #floating-point number
+complex_num = complex(3,4)  #complex number
+
+
+greeting = "Mabuhay! Welcome to Philippines"
+print(greeting.lower())
+print(greeting.replace("Philippines", "Python"))
+
+print(num1, grade, complex_num)
+
+isLearning = True
+isDifficult = False
+
+age = 18
+
+print(type(age))
+print(type(greeting))
+print(type(isDifficult))
+
+
+x = 42
+print(float(x))
+print(str(x))
+print(bool(x))
+print(int("76"))
+
+
+
+# Data Types
+# string
